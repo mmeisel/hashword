eda9fba952f41c5aab90451f3c0b9c85729cf1a8
Michael Meisel <michael@thousandeyes.com>
2014-07-29 00:30:16 -0700

    Standalone page improvements
    - Now the password will be selected automatically when the form is
      submitted for easy copy/paste. Tested on Chrome, Firefox, and
      Mobile Safari.
    - Hide password when output field is blurred.

--- index.js
+++ index.js
@@ -4,24 +4,40 @@ angular.module('index', ['siteSettings'])
 .controller('HashwordCtrl', ['$scope', function ($scope) {
     $scope.form = {};
     $scope.settings = hw.getDefaultSettings();
-    
-    $scope.updatePassword = function () {
-        var pw = hw.getHashword($scope.form.domain, $scope.form.password, $scope.settings);
-        $scope.form.output = pw;
-    };
-    
-    $scope.selectOutput = function ($event) {
-        console.log($event.target.select);
-    };
+    $scope.output = '';
 }])
-.directive('hwSelectOnSubmit', ['$timeout', function ($timeout) {
-    return function (scope, element) {
-        angular.element(element[0].form).on('submit', function () {
-            $timeout(function () {
-                element[0].focus();
-                element[0].select();
-            });
-        });
+.directive('hwSubmit', function () {
+    return function ($scope, element) {
+        var outputField = angular.element(element[0].querySelector('input.hw-output'));
+        
+        element.on('submit', function (e) {
+            e.preventDefault();
+            $scope.output = hw.getHashword($scope.form.domain, $scope.form.password, $scope.settings);
+
+            // Manually inject the value into the input field instead of relying on angular binding.
+            // iOS will not allow the field to be focused programmatically unless it's done from
+            // the event handler directly. With angular binding, we would have to use $timeout to 
+            // wait for a digest cycle to complete before there was any text to select inside the
+            // field.
+            outputField.val($scope.output);
+            outputField[0].focus();
+            // select is broken on iOS, use setSelectionRange instead.
+            outputField[0].setSelectionRange(0, outputField.val().length);
+        }); 
+        outputField.on('focus', function () {
+            outputField.attr('type', 'text');
+            outputField[0].setSelectionRange(0, outputField.val().length);
+        })  
+        .on('blur', function () {
+            // Replace the text in case it was modified (and hide it).
+            // iOS won't let us select text in a readonly input.
+            outputField.attr('type', 'password').val($scope.output);
+        })  
+        .on('mouseup', function (e) {
+            // Without this, the mouseup will move the cursor. We want to keep the selection from
+            // the focus event.
+            e.preventDefault();
+        }); 
     };
-}])
+})
 ;
