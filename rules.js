5e6cbb44fc8f0a5e632fe9c1c888e2101173048d
Michael Meisel <michael@thousandeyes.com>
2015-10-03 20:42:50 -0700

    Fancier use of icons to indicate state
    - Different icon when the site is new vs already known
    - Nicer icons

--- rules.js
+++ rules.js
@@ -5,25 +5,34 @@ var hwRules = {};
 
 hwRules.loadIcons = function(callback) {
     var imageSizes = ['19', '38'];
+    var imageVariants = ['insert', 'add'];
+    var totalImages = imageSizes.length * imageVariants.length;
+    var loadedImages = 0;
     var images = {};
     var imageData = {};
 
-    imageSizes.forEach(function(size) {
-        var image = new Image();
+    imageVariants.forEach(function (variant) {
+        images[variant] = {};
+        imageData[variant] = {};
 
-        image.src = chrome.runtime.getURL('images/icon' + size + '.png');
-        image.onload = _handleImageLoaded.bind(image, size);
-        images[size] = image;
+        imageSizes.forEach(function(size) {
+            var image = new Image();
+
+            image.src = chrome.runtime.getURL('images/icon' + size + '-' + variant + '.png');
+            image.onload = _handleImageLoaded.bind(image, variant, size);
+            images[variant][size] = image;
+        });
     });
 
-    function _handleImageLoaded(size) {
+    function _handleImageLoaded(variant, size) {
         var canvas = document.createElement("canvas");
         var ctx = canvas.getContext("2d");
 
         ctx.drawImage(this, 0, 0, size, size);
-        imageData[size] = ctx.getImageData(0, 0, size, size);
+        imageData[variant][size] = ctx.getImageData(0, 0, size, size);
+        loadedImages += 1;
 
-        if (Object.keys(imageData).length == imageSizes.length) {
+        if (loadedImages === totalImages) {
             callback(imageData);
         }
     }
@@ -35,23 +44,35 @@ hwRules.resetRules = function () {
         // we need to manually load the icon images first.
         hwRules.loadIcons(_resetRulesInternal.bind(this, hwData));
     });
-    
+
     function _resetRulesInternal(hwData, imageData) {
-        // Change the icon based on whether the extension has been used on this page before
-        var iconRule = {
-            actions: [new chrome.declarativeContent.SetIcon({ imageData: imageData })]
+        // When the page has a password field, show the "add" icon if the extension hasn't been used
+        // here before. Show the "insert" icon if it has.
+        var addIconRule = {
+            priority: 50,
+            conditions: [
+                new chrome.declarativeContent.PageStateMatcher({ css: ["input[type='password']"] })
+            ],
+            actions: [new chrome.declarativeContent.SetIcon({ imageData: imageData.add })]
         };
 
-        iconRule.conditions = Object.keys(hwData).map(function (domain) {
+        var insertIconRule = {
+            // Prefer this rule since known sites will match both.
+            priority: 100,
+            actions: [new chrome.declarativeContent.SetIcon({ imageData: imageData.insert })]
+        };
+
+        insertIconRule.conditions = Object.keys(hwData).map(function (domain) {
             // An implicit '.' is added at the beginning of the hostname, so this will work for
             // exact matches as well.
             return new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: { hostSuffix: '.' + domain }
+                pageUrl: { hostSuffix: '.' + domain },
+                css: ['input[type="password"]']
             });
         });
 
         chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-            chrome.declarativeContent.onPageChanged.addRules([iconRule]);
+            chrome.declarativeContent.onPageChanged.addRules([addIconRule, insertIconRule]);
         });
     }
 };
