bc81ad46920e76639428b32ebd1f161e1e0790a8
Michael Meisel <michael@thousandeyes.com>
2015-10-05 22:37:55 -0700

    Minor code cleanup, no functional changes

--- popup.js
+++ popup.js
@@ -3,23 +3,34 @@
 angular.module('popup', ['common', 'siteSettings'])
 .controller('PopupCtrl', ['$scope', function ($scope) {
     $scope.popup = { showSettings: false };
-    
+
     $scope.hasSubdomain = function () {
         return $scope.domainInfo && $scope.domainInfo.tld != $scope.domainInfo.name &&
             ('www.' + $scope.domainInfo.tld) != $scope.domainInfo.name;
     };
-    
+
     $scope.closeWindow = function () {
         window.close();
     };
-    
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        if (tabs.length && tabs[0].url && tabs[0].url.indexOf('http') === 0) {
+            _init(tabs[0]);
+        }
+        else {
+            $scope.popup.error = 'Hashword cannot be used on this page.';
+        }
+
+        $scope.$apply();
+    });
+
     // Update dates and save settings. We need to update a copy of the object in the
     // scope so we don't cause another digest cycle when we're trying to close the window.
     function _updateAndSaveSettings() {
         var items = {};
         var settings = angular.copy($scope.settings);
         var isNewDomain = settings.createDate == null;
-        
+
         settings.accessDate = new Date().getTime();
         if (isNewDomain) {
             settings.createDate = settings.accessDate;
@@ -32,13 +43,13 @@ angular.module('popup', ['common', 'siteSettings'])
             }
         });
     }
-    
+
     function _init(tab) {
         $scope.domainInfo = hw.getDomainInfo(tab.url);
-        
+
         $scope.insertPassword = function () {
             var pw = hw.getHashword($scope.popup.domain, $scope.popup.password, $scope.settings);
-    
+
             // Populate field, then trigger some key events so hopefully the scripts on the page
             // will register that we've entered something.
             var script = 'document.activeElement.value = ' + JSON.stringify(pw) + '; ' +
@@ -52,16 +63,15 @@ angular.module('popup', ['common', 'siteSettings'])
                 code: script,
                 allFrames: true
             });
-    
+
             _updateAndSaveSettings();
             window.close();
         };
-        
+
         $scope.changeDomain = function () {
-            $scope.settings = $scope.allSettings[$scope.popup.domain] ||
-                hw.getDefaultSettings();
+            $scope.settings = $scope.allSettings[$scope.popup.domain] || hw.getDefaultSettings();
         };
-        
+
         chrome.storage.local.get([$scope.domainInfo.name, $scope.domainInfo.tld], function (items) {
             // Only use the full hostname as the key if there are already settings for it,
             // otherwise fall back to the effective TLD. In other words, the effective TLD
@@ -74,11 +84,4 @@ angular.module('popup', ['common', 'siteSettings'])
             $scope.$apply();
         });
     }
-    
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        if (tabs.length && tabs[0].url && tabs[0].url.indexOf('http') === 0) _init(tabs[0]);
-        else $scope.popup.error = 'Hashword cannot be used on this page.';
-   
-        $scope.$apply();
-    });
 }]);
