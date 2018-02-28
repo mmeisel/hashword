a539b2b5685c199d40f0b92612fb4d7e1b4d1348
Michael Meisel <mrmeisel@gmail.com>
2018-02-28 14:28:24 +0700

    Refactor of sync code

--- src/common/sync.js
+++ src/common/sync.js
@@ -2,49 +2,60 @@
 
 angular.module('sync', [])
 .service('syncService', ['$http', function ($http) {
-  this.sync = function () {
-    return hwStorage.getOptions().then(options => {
-      if (!options.useServer) {
-        return false
-      }
+  this.getDomainsToSync = function (serverUrl) {
+    return Promise.all([
+      hwStorage.getAll(),
+      $http.get(`${serverUrl}/sites`)
+    ])
+    .then(results => {
+      const localDomainMap = results[0]
+      const remoteDomainMap = results[1].data
+      const toSync = {}
 
-      return Promise.all([
-        hwStorage.getAll(),
-        $http.get(`${options.serverUrl}/sites`)
-      ])
-      .then(results => {
-        const localDomainMap = results[0]
-        const remoteDomainMap = results[1].data
-        const toSync = {}
+      Object.keys(remoteDomainMap).forEach(domain => {
+        const local = localDomainMap[domain]
+        const remote = remoteDomainMap[domain]
 
-        Object.keys(remoteDomainMap).forEach(domain => {
-          const local = localDomainMap[domain]
-          const remote = remoteDomainMap[domain]
-
-          if (local == null) {
-            toSync[domain] = null
-          } else if (local.rev !== remote.rev || local.accessDate !== remote.accessDate) {
-            toSync[domain] = local
-          }
-        })
-
-        if (Object.keys(toSync).length) {
-          return $http.patch(`${options.serverUrl}/sites`, toSync)
-        } else {
-          return true
+        if (local == null) {
+          toSync[domain] = null
+        } else if (local.rev !== remote.rev || local.accessDate !== remote.accessDate) {
+          toSync[domain] = local
         }
       })
+
+      return toSync
+    })
+  }
+
+  this.syncDomains = function (serverUrl, domainsToSync) {
+    if (!Object.keys(domainsToSync).length) {
+      return Promise.resolve({})
+    }
+
+    // The $http call must be wrapped since it returns a $q instead of a real Promise
+    return Promise.resolve($http.patch(`${serverUrl}/sites`, domainsToSync))
       .then(response => {
         const results = response.data
 
-        if (results != null && Object.keys(results.changed).length) {
-          return hwStorage.set(results.changed)
-        } else {
-          return true
+        if (!Object.keys(results.changed).length) {
+          return domainsToSync
         }
+
+        return hwStorage.set(results.changed).then(() => domainsToSync)
         // TODO: deal with conflicts
       })
       // TODO: deal with errors
+  }
+
+  this.sync = function () {
+    return hwStorage.getOptions().then(options => {
+      if (!options.useServer) {
+        return {}
+      }
+
+      return this.getDomainsToSync(options.serverUrl).then(toSync => {
+        return this.syncDomains(options.serverUrl, toSync)
+      })
     })
   }
 }])
--- test/sync.spec.js
+++ test/sync.spec.js
@@ -19,134 +19,176 @@ describe('syncService', () => {
     $httpBackend.verifyNoOutstandingRequest()
   })
 
-  it('should do nothing when useServer is false', () => {
-    const getOptionsStub = sandbox.stub(hwStorage, 'getOptions').returns(Promise.resolve({
-      useServer: false
-    }))
-    const getAllStub = sandbox.stub(hwStorage, 'getAll').returns(Promise.resolve({}))
-    const setStub = sandbox.stub(hwStorage, 'set').returns(Promise.resolve())
+  describe('#getDomainsToSync()', () => {
+    it('should not select domains where the rev and accessDate match the server', () => {
+      const sites = {
+        'example.com': {
+          accessDate: 1516754869000,
+          rev: '22222222'
+        }
+      }
 
-    return syncService.sync().then(result => {
-      expect(getOptionsStub.calledOnce).to.equal(true)
-      expect(getAllStub.called).to.equal(false)
-      expect(setStub.called).to.equal(false)
-      expect(result).to.equal(false)
+      const stubs = setUpStubs(sites, sites)
+
+      return syncService.getDomainsToSync('http://localhost').then(toSync => {
+        expect(stubs.storageGetAll.calledOnce).to.equal(true)
+        expect(toSync).to.deep.equal({})
+      })
     })
+
+    it('should select domains where the rev matches but accessDate does not', () => {
+      const localSites = {
+        'example.com': {
+          accessDate: 1519447920000,
+          rev: '22222222'
+        }
+      }
+
+      const remoteSites = {
+        'example.com': {
+          accessDate: 1516754869000,
+          rev: '22222222'
+        }
+      }
+
+      const stubs = setUpStubs(localSites, remoteSites)
+
+      return syncService.getDomainsToSync('http://localhost').then(toSync => {
+        expect(stubs.storageGetAll.calledOnce).to.equal(true)
+        expect(toSync).to.deep.equal(localSites)
+      })
+    })
+
+    it('should select domains where the rev does not match', () => {
+      const localSites = {
+        'example.com': {
+          accessDate: 1516754869000,
+          rev: '33333333'
+        }
+      }
+
+      const remoteSites = {
+        'example.com': {
+          accessDate: 1516754869000,
+          rev: '22222222'
+        }
+      }
+
+      const stubs = setUpStubs(localSites, remoteSites)
+
+      return syncService.getDomainsToSync('http://localhost').then(toSync => {
+        expect(stubs.storageGetAll.calledOnce).to.equal(true)
+        expect(toSync).to.deep.equal(localSites)
+      })
+    })
+
+    function setUpStubs (localSites, remoteSites) {
+      const stubs = {
+        storageGetAll: sandbox.stub(hwStorage, 'getAll').returns(Promise.resolve(localSites)),
+        syncDomains: sandbox.stub(syncService, 'syncDomains').returns(Promise.resolve(localSites))
+      }
+
+      $httpBackend.whenGET('http://localhost/sites').respond(remoteSites)
+      setTimeout(() => $httpBackend.flush(), 0)
+
+      return stubs
+    }
   })
 
-  it('should save changes from the server', () => {
-    const localSites = {
-      'example.com': {
-        history: ['00000000'],
-        id: undefined,
-        userId: undefined,
-        domain: undefined,
-        accessDate: 1516754869000,
-        createDate: 1516754869000,
-        deleteDate: null,
-        generation: 1,
-        pwLength: 16,
-        symbols: true,
-        notes: '',
-        rev: '11111111'
+  describe('#syncDomains()', () => {
+    it('should save changes from the server', () => {
+      const localSites = {
+        'example.com': {
+          history: ['00000000'],
+          accessDate: 1516754869000,
+          createDate: 1516754869000,
+          deleteDate: null,
+          generation: 1,
+          pwLength: 16,
+          symbols: true,
+          notes: '',
+          rev: '11111111'
+        }
       }
-    }
 
-    const remoteSites = {
-      'example.com': {
-        history: ['00000000', '11111111'],
-        id: undefined,
-        userId: undefined,
-        domain: undefined,
-        accessDate: 1516754869000,
-        createDate: 1516754869000,
-        deleteDate: null,
-        generation: 1,
-        pwLength: 16,
-        symbols: true,
-        notes: '',
-        rev: '22222222'
+      const remoteSites = {
+        'example.com': {
+          history: ['00000000', '11111111'],
+          accessDate: 1519447920000,
+          createDate: 1516754869000,
+          deleteDate: null,
+          generation: 2,
+          pwLength: 16,
+          symbols: true,
+          notes: '',
+          rev: '22222222'
+        }
       }
+
+      const stubs = setUpStubs(localSites, remoteSites, {
+        accepted: [],
+        rejected: {},
+        changed: remoteSites
+      })
+
+      return syncService.syncDomains('http://localhost', localSites).then(() => {
+        expect(stubs.storageSet.calledOnce).to.equal(true)
+        expect(stubs.storageSet.getCalls()[0].args).to.deep.equal([remoteSites])
+      })
+    })
+
+    function setUpStubs (localSites, remoteSites, response) {
+      const stubs = {
+        storageSet: sandbox.stub(hwStorage, 'set').returns(Promise.resolve())
+      }
+
+      $httpBackend.whenPATCH('http://localhost/sites').respond(response)
+      setTimeout(() => $httpBackend.flush(), 0)
+
+      return stubs
     }
-
-    const stubs = setUpStubs(localSites, remoteSites)
-
-    $httpBackend.whenPATCH('http://localhost/sites').respond({
-      accepted: [],
-      rejected: {},
-      changed: remoteSites
-    })
-    setTimeout(() => $httpBackend.flush(), 0)
-
-    return syncService.sync().then(() => {
-      expect(stubs.storageGetAll.calledOnce).to.equal(true)
-      expect(stubs.storageGetOptions.calledOnce).to.equal(true)
-
-      expect(stubs.storageSet.calledOnce).to.equal(true)
-      expect(stubs.storageSet.getCalls()[0].args).to.deep.equal([remoteSites])
-    })
   })
 
-  it('should not send sites to the server when the rev and accessDate are the same', () => {
-    const localSites = {
-      'example.com': {
-        history: ['00000000', '11111111'],
-        id: undefined,
-        userId: undefined,
-        domain: undefined,
-        accessDate: 1516754869000,
-        createDate: 1516754869000,
-        deleteDate: null,
-        generation: 1,
-        pwLength: 16,
-        symbols: true,
-        notes: '',
-        rev: '22222222'
-      }
-    }
+  describe('#sync()', () => {
+    it('should do nothing when useServer is false', () => {
+      const getOptionsStub = sandbox.stub(hwStorage, 'getOptions').returns(Promise.resolve({
+        useServer: false
+      }))
+      const getDomainsStub = sandbox.stub(syncService, 'getDomainsToSync').returns(Promise.resolve({}))
+      const syncDomainsStub = sandbox.stub(syncService, 'syncDomains').returns(Promise.resolve({}))
 
-    const remoteSites = {
-      'example.com': {
-        history: ['00000000', '11111111'],
-        id: undefined,
-        userId: undefined,
-        domain: undefined,
-        accessDate: 1516754869000,
-        createDate: 1516754869000,
-        deleteDate: null,
-        generation: 1,
-        pwLength: 16,
-        symbols: true,
-        notes: '',
-        rev: '22222222'
-      }
-    }
-
-    const stubs = setUpStubs(localSites, remoteSites)
-
-    return syncService.sync().then(result => {
-      expect(result).to.equal(true)
-      expect(stubs.storageSet.called).to.equal(false)
+      return syncService.sync().then(result => {
+        expect(getOptionsStub.calledOnce).to.equal(true)
+        expect(getDomainsStub.called).to.equal(false)
+        expect(syncDomainsStub.called).to.equal(false)
+        expect(result).to.deep.equal({})
+      })
     })
-  })
 
-  function setUpStubs (localSites, remoteSites) {
-    const stubs = {
-      storageGetOptions: sandbox.stub(hwStorage, 'getOptions').returns(Promise.resolve({
+    it('should call syncDomains with the results of getDomainsToSync', () => {
+      const localSites = {
+        'example.com': {
+          accessDate: 1519447920000,
+          rev: '22222222'
+        }
+      }
+
+      const getOptionsStub = sandbox.stub(hwStorage, 'getOptions').returns(Promise.resolve({
         useServer: true,
         serverUrl: 'http://localhost'
-      })),
-      storageGetAll: sandbox.stub(hwStorage, 'getAll').returns(Promise.resolve(localSites)),
-      storageSet: sandbox.stub(hwStorage, 'set').returns(Promise.resolve())
-    }
+      }))
+      const getDomainsStub = sandbox.stub(syncService, 'getDomainsToSync').returns(Promise.resolve(localSites))
+      const syncDomainsStub = sandbox.stub(syncService, 'syncDomains').returns(Promise.resolve(localSites))
 
-    $httpBackend.whenGET('http://localhost/sites').respond(remoteSites)
-
-    // A flush for the GET request -- the PATCH will need a second one
-    setTimeout(() => $httpBackend.flush(), 0)
-
-    return stubs
-  }
+      return syncService.sync().then(result => {
+        expect(getOptionsStub.calledOnce).to.equal(true)
+        expect(getDomainsStub.calledOnce).to.equal(true)
+        expect(syncDomainsStub.calledOnce).to.equal(true)
+        expect(syncDomainsStub.getCalls()[0].args).to.deep.equal(['http://localhost', localSites])
+        expect(result).to.deep.equal(localSites)
+      })
+    })
+  })
 })
 
 afterEach(() => sandbox.restore())
